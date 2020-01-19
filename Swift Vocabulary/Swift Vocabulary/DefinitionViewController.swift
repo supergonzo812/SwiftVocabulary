a726a5581a806073841bcaaff9dbd7b52d894a84
Chris Gonzales <57194564+supergonzo812@users.noreply.github.com>
2020-01-19 07:56:19 -0800

    MVP complete, began code for alert button to add new term and definition

--- Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -22,21 +22,9 @@ class DefinitionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
-        // Do any additional setup after loading the view.
     }
     
     @IBOutlet weak var SwiftTerm: UILabel!
-    
     @IBOutlet weak var Definition: UITextView!
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Swift Vocabulary/Swift Vocabulary/WordsTableViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/WordsTableViewController.swift
@@ -15,92 +15,75 @@ class WordsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+      
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+
         return vocabWords.count
         
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
-
-        // Configure the cell...
-        
         let text = vocabWords[indexPath.row]
         cell.textLabel?.text = text.word
-
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         if segue.identifier == "ShowDefinitionSegue"{
-                   
             if let indexPath = tableView.indexPathForSelectedRow, let definitionVC = segue.destination as? DefinitionViewController{
                 let term = vocabWords[indexPath.row]
-                
                 definitionVC.vocabWord = term
             }
-            
-            
-            
         }
-        
     }
     
-
+    
+ 
+        @IBAction func AddTerm(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "Add a new term", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addTextField(configurationHandler: {UITextField in UITextField.placeholder = "New Term"})
+            self.present(alert, animated: true)
+        alert.addAction(UIAlertAction(title: "Add Term", style: .default, handler: {
+            action in
+            if let newTerm = alert.textFields?.first?.text{
+                for term in self.vocabWords{
+                    guard newTerm == term.word else{
+                        let errorAlert = UIAlertController(title: "Error", message: "Term already in dictionary", preferredStyle: UIAlertController.Style.alert)
+                        errorAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                        
+                        for _ in newTerm{
+                        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                        alert.addTextField(configurationHandler: {UITextField in UITextField.placeholder = "Definition"})
+                        self.present(alert, animated: true)
+                        alert.addAction(UIAlertAction(title: "Add Definition", style: .default, handler: {
+                            
+                            action in
+                            if var newDefintion = alert.textFields?.first?.text{
+                                for term in self.vocabWords{
+                                    newDefintion = term.definition
+                                    return
+                                }
+                                }
+                                }
+                            )
+                            )
+                                
+                        }
+                            
+                            
+                        return
+                        self.vocabWords.append(VocabularyWord(word: "\(newTerm)", definition: "\(term.definition)"))
+                            }
+                }}}))
+    
+}
 }
